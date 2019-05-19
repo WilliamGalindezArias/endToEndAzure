f1247c82c3c8d90668c4bd199a19f458651ae789
WilliamArias12 <william.galindez.arias@gmail.com>
2019-05-19 22:56:21 +0200

    Modified the load_model function and changed the weights

--- convnetwork.py
+++ convnetwork.py
@@ -3,14 +3,14 @@ from keras import backend
 import numpy as np
 
 
-def load_model():
+def load_model_custom():
 	return load_model('trained_model/model_cnn_1.h5')
 
 
 def predict(img):
 	test=(255-img.reshape(1,28,28,1).astype('float32'))/255
     # Predict from Keras
-	one_hot = load_model().predict(test)
+	one_hot = load_model_custom().predict(test)
 	backend.clear_session()
 	return np.argmax(one_hot)
 
